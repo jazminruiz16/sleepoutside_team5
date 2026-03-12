5f5da89f4943446a0ba5d8f688c7943c4884f84a
redheadw <wrightinthered@att.net>
2026-03-11 19:21:28 -0500

    Add Vite updates and product listing work

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,42 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    const cart = getLocalStorage("so-cart") || [];
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
+  }
+
+  renderProductDetails() {
+    document.getElementById("productBrand").textContent = this.product.Brand.Name;
+    document.getElementById("productName").textContent =
+      this.product.NameWithoutBrand;
+    document.getElementById("productImage").src = this.product.Image;
+    document.getElementById("productImage").alt =
+      this.product.NameWithoutBrand;
+    document.getElementById("productPrice").textContent =
+      `$${this.product.FinalPrice}`;
+    document.getElementById("productColor").textContent =
+      this.product.Colors[0].ColorName;
+    document.getElementById("productDescription").innerHTML =
+      this.product.DescriptionHtmlSimple;
+
+    document.getElementById("addToCart").dataset.id = this.product.Id;
+    document.title = `Sleep Outside | ${this.product.Name}`;
+  }
+}
