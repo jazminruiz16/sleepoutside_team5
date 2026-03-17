1410a9aabb261208f3b7ab48c464e759a9ae3164
jazminruiz16 <jazmingruiz@gmail.com>
2026-03-16 22:59:19 -0400

    w02 index html

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,13 +1,13 @@
 import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
     return `<li class="product-card">
-        <a href="product_pages/?product=">
+        <a href="product_pages/?product= ${product.Id}">
             <img src="${product.Image}" alt="Image of ${product.Name}">
             <h2 class="card__brand">${product.Brand.Name}</h2>
             <h3 class="card__name">${product.Name}</h3>
             <p class="product-card__price">$${product.FinalPrice}</p>
         </a>
-    </li>`
+    </li>`;
 }
 export default class ProductList {
     constructor(category, dataSource, listElement) {
